17c3e30ea42fe124bf48860ff24b80e1807f80be
Leonardo <sonleo2013@gmail.com>
2019-11-16 19:02:55 -0300

    recuperando nome de usuário

--- js/corretoErrado.js
+++ js/corretoErrado.js
@@ -1,5 +1,6 @@
-
-
+//Setando o nome dos players nas divs
+document.getElementById("name1").innerHTML = localStorage.getItem("player1");
+document.getElementById("name2").innerHTML = localStorage.getItem("player2");
 
 function acertou(){
 	var m = document.getElementsById("atv1"); 
@@ -9,6 +10,7 @@ function acertou(){
 	c.width = "200px";
 	c.height = "100px";
 	c.borderColor = "#f00";
+
 }
 
 function errou(){
--- js/pegarValor.js
+++ js/pegarValor.js
@@ -1,10 +1,24 @@
-var capturando ="";
+// var capturando ="";
 
-function capturar () {
-    capturando = document.getElementById('jg1').value;
-}
+// function capturar () {
+//     capturando = document.getElementById('jg1').value;
+// }
 
 
-function mudaNome () {
-	document.getElementById('fodaa').innerHTML = capturando;
-}
+// function mudaNome () {
+// 	document.getElementById('fodaa').innerHTML = capturando;
+// }
+
+const btplay = document.getElementById('btPlay');
+
+var nome1;
+var nome2;
+
+btplay.addEventListener('click', ()=>{
+    //Pegando os nomes dos dois players
+    nome1 = document.getElementById("jg1").value;
+    nome2 = document.getElementById("jg2").value;
+    //Salvando o nome dos dois players no browser
+    localStorage.setItem("player1",nome1);
+    localStorage.setItem("player2",nome2);
+})
