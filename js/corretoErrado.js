f97b445d56ecfd22434c7ba08321a190bd9709db
Alexandre <lucasdeoliveirag1@gmail.com>
2019-11-16 18:18:37 -0400

    v1

---
+++ js/corretoErrado.js
@@ -0,0 +1,16 @@
+
+
+
+function acertou(){
+	var m = document.getElementsById("atv1"); 
+	c = m.style;
+	c.color = "#c00";
+	c.backgroundColor = "#eee";
+	c.width = "200px";
+	c.height = "100px";
+	c.borderColor = "#f00";
+}
+
+function errou(){
+}
+
---
+++ js/pegarValor.js
@@ -0,0 +1,10 @@
+var capturando ="";
+
+function capturar () {
+    capturando = document.getElementById('jg1').value;
+}
+
+
+function mudaNome () {
+	document.getElementById('fodaa').innerHTML = capturando;
+}
