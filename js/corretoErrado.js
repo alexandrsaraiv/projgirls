bd6136fbb9b79508a2fdfc0a4dda6c737ed82f3e
Alexandre <lucasdeoliveirag1@gmail.com>
2019-12-07 11:36:08 -0400

    atv3 adicionada

--- js/corretoErrado.js
+++ js/corretoErrado.js
@@ -2,6 +2,7 @@
 document.getElementById("name1").innerHTML = localStorage.getItem("player1");
 document.getElementById("name2").innerHTML = localStorage.getItem("player2");
 
+var vez=0,vez_atv3=0,vez_atv4=0;
 
 //AUMENTA PONTUAÇÃO DO JOGADOR 1
 function play1_score(){
@@ -38,7 +39,7 @@ function reset(){
 function fimAtv1(){
 	document.getElementById('wrong').style.display="none";
 	document.getElementById('check').style.display="none";
-	document.getElementById('divFinal').style5.display="table-cell";
+	document.getElementById('divFinal').style.display="table-cell";
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////FIM DA ATIVIDADE 1////////////////////////////////////////////////////
@@ -55,21 +56,195 @@ function audio(){
 }
 //VERIFICA SE O DIGITADO É A PALAVRA CORRETA
 function verificaAtv2(){
+	vez++;
 	digitado = document.getElementById("resp-atv2").value;
-	if(digitado.toLowerCase() == 'palavra'){
+	if(digitado.toLowerCase() == 'atrapalhado'){
 		if(vez==1){
 			play1_score();
+			acertouAtv2();
 		}
 		if(vez==2){
 			play2_score();
+			acertouAtv2();
+			alert('Resposta Correta, Finalizando atividade!');
+			fimAtv2();
+		}
+	}else{
+		if(vez==1){
+			errouAtv2();
+		}
+		if(vez==2){
+			errouAtv2();
+			alert('Resposta errada, Finalizando atividade!');
+			fimAtv2();
 		}
 	}
+}
+
+function acertouAtv2(){
+	document.getElementById('digitar-palavra').style.display="none";
+	document.getElementById('check2').style.display="table-cell";
+}
+
+function errouAtv2(){
+	document.getElementById('digitar-palavra').style.display="none";
+	document.getElementById('wrong2').style.display="table-cell";
+}
+
+function reset2(){
+	document.getElementById('wrong2').style.display="none";
+	document.getElementById('check2').style.display="none";
+	document.getElementById('digitar-palavra').style.display="table-cell";
+}
+
+function fimAtv2(){
+	document.getElementById('wrong2').style.display="none";
+	document.getElementById('check2').style.display="none";
+	document.getElementById('fimAtv2').style.display="table-cell";
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////FIM DA ATIVIDADE 2////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
+/////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////ATIVIDADE 3///////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+function atv3(){
+
+if (window.SpeechRecognition || window.webkitSpeechRecognition) {
+				vez_atv3++;
+				console.log(vez_atv3);
+                // captura a voz
+                var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+                var recognition = new SpeechRecognition();
+                // inicia reconhecimento
+                recognition.start();
+                // resultado do reconhecimento
+                recognition.addEventListener('result', function(e) {
+                    console.log(e);
+                    var result = e.results[0][0].transcript;
+					console.log(result);
+					if(result.toLowerCase() == 'chave'){
+						if(vez_atv3==1){
+							play1_score();
+							acertouAtv3();
+						}
+						if(vez_atv3==2){
+							play2_score();
+							acertouAtv3();
+							fimAtv3();
+						}
+					}else{
+						if(vez_atv3==1){
+							errouAtv3();
+						}
+						if(vez_atv3==2){
+							errouAtv3();
+							fimAtv3();
+						}
+					}
 
 
+                }, false);
+            } else {
+                alert('Este navegador não suporta esta funcionalidade ainda!');
+			}
+}
+function acertouAtv3(){
+	document.getElementById('atv3-jogar').style.display="none";
+	document.getElementById('check3').style.display="table-cell";
+}
 
+function errouAtv3(){
+	document.getElementById('atv3-jogar').style.display="none";
+	document.getElementById('wrong3').style.display="table-cell";
+}
+
+function reset3(){
+	document.getElementById('wrong3').style.display="none";
+	document.getElementById('check3').style.display="none";
+	document.getElementById('atv3-jogar').style.display="table-cell";
+}
+
+function fimAtv3(){
+	document.getElementById('wrong3').style.display="none";
+	document.getElementById('check3').style.display="none";
+	document.getElementById('fimAtv3').style.display="table-cell";
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////FIM DA ATIVIDADE 3////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////ATIVIDADE 4///////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
+function atv3(){
+
+	if (window.SpeechRecognition || window.webkitSpeechRecognition) {
+					vez_atv4++;
+					console.log(vez_atv4);
+					// captura a voz
+					var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+					var recognition = new SpeechRecognition();
+					// inicia reconhecimento
+					recognition.start();
+					// resultado do reconhecimento
+					recognition.addEventListener('result', function(e) {
+						console.log(e);
+						var result = e.results[0][0].transcript;
+						console.log(result);
+						if(result.toLowerCase() == 'chave'){
+							if(vez_atv4==1){
+								play1_score();
+								acertouAtv4();
+							}
+							if(vez_atv4==2){
+								play2_score();
+								acertouAtv4();
+								fimAtv4();
+							}
+						}else{
+							if(vez_atv4==1){
+								errouAtv4();
+							}
+							if(vez_atv4==2){
+								errouAtv4();
+								fimAtv4();
+							}
+						}
+	
+	
+					}, false);
+				} else {
+					alert('Este navegador não suporta esta funcionalidade ainda!');
+				}
+	}
+	function acertouAtv3(){
+		document.getElementById('atv3-jogar').style.display="none";
+		document.getElementById('check3').style.display="table-cell";
+	}
+	
+	function errouAtv3(){
+		document.getElementById('atv3-jogar').style.display="none";
+		document.getElementById('wrong3').style.display="table-cell";
+	}
+	
+	function reset3(){
+		document.getElementById('wrong3').style.display="none";
+		document.getElementById('check3').style.display="none";
+		document.getElementById('atv3-jogar').style.display="table-cell";
+	}
+	
+	function fimAtv3(){
+		document.getElementById('wrong3').style.display="none";
+		document.getElementById('check3').style.display="none";
+		document.getElementById('fimAtv3').style.display="table-cell";
+	}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////FIM DA ATIVIDADE 4////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
