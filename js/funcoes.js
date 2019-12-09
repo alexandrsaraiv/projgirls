231a6342a7bced7642ecf5af33dce92b4e74d020
Alexandre <lucasdeoliveirag1@gmail.com>
2019-12-09 18:07:05 -0400

    deus me acuda

--- js/funcoes.js
+++ js/funcoes.js
@@ -2,8 +2,8 @@
 document.getElementById("name1").innerHTML = localStorage.getItem("player1");
 document.getElementById("name2").innerHTML = localStorage.getItem("player2");
 
-var vez=0,vez_atv3=0,vez_atv4=0;
-
+var vez=0,vez_atv3=0,vez_atv4=0,vez_atv5=0;
+var final1, final1;
 //AUMENTA PONTUAÇÃO DO JOGADOR 1
 function play1_score(){
 	var score1 = parseInt(document.getElementById('score1').innerText)+100;
@@ -14,6 +14,13 @@ function play1_score(){
 function play2_score(){
 	var score2 = parseInt(document.getElementById('score2').innerText)+100;
 	parseInt(document.getElementById('score2').innerHTML = score2);
+}
+
+function finaliza(){
+	final1 = document.getElementById("score1").value;
+	final2 = document.getElementById("score2").value;
+	localStorage.setItem("score1fim",final1);
+    localStorage.setItem("score2fim",final2);
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
@@ -51,7 +58,11 @@ function fimAtv1(){
 /////////////////////////////////////////////////////////////////////////////////////////////////
 //AUDIO ATIVIDADE 2
 function audio(){
-	const audio = document.querySelector('audio');
+	const audio = document.querySelector('#audio2');
+	audio.play();
+}
+function audio2(){
+	const audio = document.querySelector('#audio5');
 	audio.play();
 }
 //VERIFICA SE O DIGITADO É A PALAVRA CORRETA
@@ -134,6 +145,7 @@ if (window.SpeechRecognition || window.webkitSpeechRecognition) {
 						if(vez_atv3==2){
 							play2_score();
 							acertouAtv3();
+							alert('Resposta Correta, Finalizando atividade!');
 							fimAtv3();
 						}
 					}else{
@@ -142,6 +154,7 @@ if (window.SpeechRecognition || window.webkitSpeechRecognition) {
 						}
 						if(vez_atv3==2){
 							errouAtv3();
+							alert('Resposta Errada, Finalizando atividade!');
 							fimAtv3();
 						}
 					}
@@ -202,6 +215,7 @@ function atv4(){
 							if(vez_atv4==2){
 								play2_score();
 								acertouAtv4();
+								alert('Resposta Correta, Finalizando atividade!');
 								fimAtv4();
 							}
 						}else{
@@ -210,6 +224,7 @@ function atv4(){
 							}
 							if(vez_atv4==2){
 								errouAtv4();
+								alert('Resposta Errada, Finalizando atividade!');
 								fimAtv4();
 							}
 						}
@@ -242,4 +257,71 @@ function atv4(){
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////FIM DA ATIVIDADE 4////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////ATIVIDADE 5///////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
+function atv5(){
+
+	if (window.SpeechRecognition || window.webkitSpeechRecognition) {
+					vez_atv5++;
+					console.log(vez_atv5);
+					var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+					var recognition = new SpeechRecognition();
+					recognition.start();
+					recognition.addEventListener('result', function(e) {
+						console.log(e);
+						var result = e.results[0][0].transcript;
+						console.log(result);
+						if(result.toLowerCase() == 'a vida é boa eu vivo à toa onde eu nasci'){
+							if(vez_atv5==1){
+								play1_score();
+								acertouAtv5();
+							}
+							if(vez_atv5==2){
+								play2_score();
+								acertouAtv5();
+								alert('Resposta Correta, Finalizando atividade!');
+								fimAtv5();
+							}
+						}else{
+							if(vez_atv5==1){
+								errouAtv5();
+							}
+							if(vez_atv5==2){
+								errouAtv5();
+								alert('Resposta Errada, Finalizando atividade!');
+								fimAtv5();
+							}
+						}
+					}, false);
+				} else {
+					alert('Este navegador não suporta esta funcionalidade ainda!');
+				}
+	}
+	function acertouAtv5(){
+		document.getElementById('atv5-jogar').style.display="none";
+		document.getElementById('check5').style.display="table-cell";
+	}
+	
+	function errouAtv5(){
+		document.getElementById('atv5-jogar').style.display="none";
+		document.getElementById('wrong5').style.display="table-cell";
+	}
+	
+	function reset5(){
+		document.getElementById('wrong5').style.display="none";
+		document.getElementById('check5').style.display="none";
+		document.getElementById('atv5-jogar').style.display="table-cell";
+	}
+	
+	function fimAtv5(){
+		document.getElementById('wrong5').style.display="none";
+		document.getElementById('check5').style.display="none";
+		document.getElementById('fimAtv5').style.display="table-cell";
+	}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////FIM DA ATIVIDADE 5////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////
--- js/pegarValor.js
+++ js/pegarValor.js
@@ -1,8 +1,11 @@
+document.getElementById("score1Fim").innerHTML = localStorage.getItem("score1fim");
+document.getElementById("score2Fim").innerHTML = localStorage.getItem("score2fim");
+
 
 const btplay = document.getElementById('btPlay');
 
-var nome1;
-var nome2;
+var nome1,ponto1;
+var nome2,ponto2;
 
 btplay.addEventListener('click', ()=>{
     //Pegando os nomes dos dois players
